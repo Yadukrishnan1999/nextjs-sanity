7ef34133c4899f4dadb9641ee2ec7b39ddacc31f
Yadukrishnan1999 <ps.yadukrishnan@litmus7.com>
2025-06-06 19:09:00 +0530

    removed unused imports

--- components/ViewAllButton.stories.ts
+++ components/ViewAllButton.stories.ts
@@ -1,7 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/nextjs";
 
-import { fn } from "storybook/test";
-
 import ViewAllButton from "./ViewAllButton";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
